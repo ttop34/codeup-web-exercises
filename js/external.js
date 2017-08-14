bd5cba7334c1d21012c3b06df54583e5042b3ed1
brose <ttop34@icloud.com>
2017-08-14 16:59:40 -0500

    #created new files external_js and inline_js
    *added a console.log to inline js
    
    *added a link to html body and console.log to external.js

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("hello from external javascript")
+
+alert("welcome to my website!");
+
+
+
+var response = prompt("what is your favorite color");
+console.log(response);
+alert("great! " + response + " is my favorite too");
