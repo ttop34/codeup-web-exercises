e48b74a6fa8880b1a4ec7685d92dc7401ef7cbbd
Elvin <ttop34@icloud.com>
2017-09-01 16:08:23 -0500

    finished jquery faq exercise

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,24 +1,57 @@
-var str = "",x,y;
+// var str = "",x,y;
+//
+// for (var i =1; i <=100; i++) {
+//     x = i%3 ==0;
+//     y = i%5 ==0;
+//
+//     if (x){
+//         str+= 'fizz';
+//     }
+//
+//     if (y) {
+//         str+= 'buzz';
+//     }
+//
+//     if (!(x||y)) {
+//         str+=i;
+//     }
+//     str+='\n';
+//
+//
+// }
+// console.log(str);
 
-for (var i =1; i <=100; i++) {
-    x = i%3 ==0;
-    y = i%5 ==0;
-
-    if (x){
-        str+= 'fizz';
-    }
-
-    if (y) {
-        str+= 'buzz';
-    }
-
-    if (!(x||y)) {
-        str+=i;
-    }
-    str+='\n';
-
-
-}
-console.log(str);
-
+//
+// function arrayMadness(fArray , lArray){
+//     var sum = 0;
+//     for (var i = 0; i < fArray.length; i++){
+//         var sqrt = Math.pow(fArray[i], 2);
+//         sum = sum += sqrt;
+//     }
+//     var summed = 0;
+//       for (var j = 0; j < lArray.length; j++) {
+//         var cube = Math.pow(lArray[j], 3);
+//         summed = summed += cube;
+//     }
+//     if (sum > summed) {
+//         return true
+//     } else if (sum <= summed) {
+//         return false
+//     } else {
+//         alert("Stop trying to break my code!");
+//     }
+// }
+//
+//
+// //no idea on this one... spent too much time talking about clearances lol//
+// function countMe(numbers){
+//
+//
+// }
+//
+//
+// function getFullName(name){
+//     var name = name.split(" ");
+//     return name[0] + " " + name[1];
+// }
 
