c1138db9d4e0d4239d2497bfcfc3c46c3a19f955
Elvin <ttop34@icloud.com>
2017-09-05 10:55:04 -0500

    finsihed traversing exercise on the jquery faq html

--- js/while.js
+++ js/while.js
@@ -5,23 +5,22 @@ while (i < 65536) {
     console.log(i)
 }
 
-// This is how you get a random number between 50 and 100
 
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// var remainingCones = allCones;
-//
-// do {
-//
-// // This is how you get a random number between 1 and 5
-//     var conesSold = Math.floor(Math.random() * 5) + 1;
-//
-//     if (conesSold > remainingCones) {
-//         console.log('Cannot sell you ' + conesSold + " i only have " + remainingCones);
-//     }
-//     else {
-//         remainingCones = remainingCones - conesSold;
-//         console.log(conesSold + " cones sold!");
-//     }
-// } while (remainingCones > 0);
-// console.log('out of cones');
+var allCones = Math.floor(Math.random() * 50) + 50;
+var remainingCones = allCones;
+
+do {
+
+// This is how you get a random number between 1 and 5
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+
+    if (conesSold > remainingCones) {
+        console.log('Cannot sell you ' + conesSold + " i only have " + remainingCones);
+    }
+    else {
+        remainingCones = remainingCones - conesSold;
+        console.log(conesSold + " cones sold!");
+    }
+} while (remainingCones > 0);
+console.log('out of cones');
 
